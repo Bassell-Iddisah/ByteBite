312ecc0a8d4784e13e1fed787a59b273552dc513
Bassell-Iddisah <bbasssell16@gmail.com>
2025-07-04 02:07:59 +0000

    fix: Resolve h2-console page for testing

--- auth-service/src/main/java/com/bytebite/auth_service/security/SecurityConfig.java
+++ auth-service/src/main/java/com/bytebite/auth_service/security/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**","/h2-console/**").permitAll()
